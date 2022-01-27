766b4f4c9169abe4a0bf5614a28e0841dcd25a4f
samuel-bazinet <bazinet.samuel1@gmail.com>
2022-01-26 20:34:07 -0500

    Added second algo for optimizing

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 # Crossword_Scrathcer_Optimize 
 
-from cgi import test
-
-
-def quicksort_letters(letters: dict[str, int]) -> list[str]:
+# quicksort_tuple_list is a function that sets up the quicksort_tuple_list_H function 
+# it is meant to sort the letters by occurence
+def quicksort_tuple_list(letters: dict[str, int]) -> list[(str, int)]:
 
     letter_items = letters.items()
     letter_list = [(i[0], i[1]) for i in letter_items]
 
-    return quicksort_letter_H(letter_list)
+    return quicksort_tuple_list_H(letter_list)
 
-def quicksort_letter_H(letters: list[(str, int)]) -> list[str]:
+# quicksort_tuple_list_H sorts a list whose elements are tuples
+# the int compared is the second element in the tuple
+def quicksort_tuple_list_H(letters: list[(str, int)]) -> list[(str, int)]:
     
     if (len(letters) <= 1):
         return letters
@@ -28,8 +29,8 @@ def quicksort_letter_H(letters: list[(str, int)]) -> list[str]:
     letters[x] = letters[-1]
     letters[-1] = temp
 
-    lista = quicksort_letter_H(letters[0:x])
-    listb = quicksort_letter_H(letters[x+1:])
+    lista = quicksort_tuple_list_H(letters[0:x])
+    listb = quicksort_tuple_list_H(letters[x+1:])
             
     return  lista + [letters[x]] + listb
 
@@ -38,7 +39,7 @@ def quicksort_letter_H(letters: list[(str, int)]) -> list[str]:
 class Board:
 
     # The class is constructed by having the user input the words contained in the crossword board
-    def __init__(self, word_count: int = 5, letter_count: int = 10):
+    def __init__(self, word_count: int = 5, letter_count: int = 10) -> None:
 
         self.words: list[str] = [input("Enter word number %d: "% a) for a in range(1, word_count+1)]
         self.letter_count = letter_count
@@ -48,7 +49,7 @@ class Board:
         self.uncovered_words_osw = 0
 
         self.optmize_char_count()        
-        #self.optimize_small_words()
+        self.optimize_small_words()
 
 
     # optimize_char_count computes the best letters by counting the total amount of letter 
@@ -66,16 +67,18 @@ class Board:
                     character_occurence[char] = 1
 
         # the letters are sorted by recurrence in increasing order
-        sorted_list = quicksort_letters(character_occurence)
+        sorted_list = quicksort_tuple_list(character_occurence)
 
         # the most occuring characters are set as the "optimal letters" in decreasing order
         for i in range(1, self.letter_count+1):
             self.letters_occ.append(sorted_list[-i][0])
 
 
+        # We use a loop too find out how many words would be found using those letters
         for i in self.words:
             flag = True
             for j in i:
+                # if one of the letters isn't in the "optimized" list, it is set as false and the loop proceeds to the next word
                 if (j not in self.letters_occ):
                     flag = False
                     break
@@ -89,15 +92,40 @@ class Board:
     # optimize_small_words computes the best letters by choosing letters that would complete
     # small words as to complete as many words as possible
     def optimize_small_words(self) -> None:
-        pass
+        
+        # the quicksort function is used to sort the words by length
+        sorted_words = quicksort_tuple_list_H([(i, len(i)) for i in self.words])
+        counter = 0
+
+        # the letters in the words are checked and added to 
+        while (len(self.letters_osw) != self.letter_count) and (counter < len(sorted_words)):
+
+            for i in sorted_words[counter][0]:
+                if i not in self.letters_osw:
+                    self.letters_osw.append(i)
+
+            counter += 1
+
+        # We use a loop too find out how many words would be found using those letters
+        for i in self.words:
+            flag = True
+            for j in i:
+                # if one of the letters isn't in the "optimized" list, it is set as false and the loop proceeds to the next word
+                if (j not in self.letters_osw):
+                    flag = False
+                    break
+
+            if flag:
+                self.uncovered_words_osw += 1
 
     
-def main():
-    test_board = Board(int(input("How many words are there? ")),18)
-    print("The optimal letters are: ", test_board.letters_occ)
-    print("There are %d words that have been uncovered with those letters"% test_board.uncovered_words_occ)
-
-    pass
+def main() -> None:
+    
+    test_board = Board(int(input("How many words are there? ")), int(input("How many letters are available? ")))
+    print("The optimal letters using occurence are: ", test_board.letters_occ)
+    print("There are %d words that have been uncovered with those letters" % test_board.uncovered_words_occ)
+    print("The optimal letters using word length are: ", test_board.letters_osw)
+    print("There are %d words that have been uncovered with those letters" % test_board.uncovered_words_osw)
 
 if __name__ == "__main__":
     main()
